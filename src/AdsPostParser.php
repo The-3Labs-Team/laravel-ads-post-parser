0d648afc93305255750dafc2f1ed0bcde57feba0
Stefano Novelli <murdercode@gmail.com>
2024-07-01 13:02:44 +0200

    Updated required PHP version to include 8.2 and 8.3, and Illuminate/contracts to include version 11.0

--- src/AdsPostParser.php
+++ src/AdsPostParser.php
@@ -43,35 +43,37 @@ class AdsPostParser
     /**
      * Append a single advertising
      */
-    public function appendSingleAdvertising(int $index, int $advIndex): string
-    {
-        $items = $this->dom->find('#adv__parsed__content > *');
-        $maxLoop = count($items);
+public function appendSingleAdvertising(int $index, int $advIndex): string
+{
+    $items = $this->dom->find('#adv__parsed__content > *');
+    $maxLoop = count($items);
 
-        if ($index >= $maxLoop) {
-            return $this->dom->save();
-        }
-
-        $currentItem = $items[$index];
-        $nextItem = $items[$index + 1] ?? null;
-
-        // If the current item is not blacklisted and the next item is not
-        // blacklisted and the next item is an HTML tag, then append the
-        // advertising code to the current item.
-        if (
-            ! preg_match($this->blacklist, $currentItem->outertext) &&
-            (! $nextItem || ! preg_match($this->blacklist, $nextItem->outertext)) &&
-            (! $nextItem || preg_match('/<\w+/', $nextItem->outertext))
-        ) {
-            $currentItem->outertext .= Blade::render('ads-post-parser::ads'.$advIndex);
-        } else {
-            // Otherwise, append the advertising code to the next item.
-            $this->appendSingleAdvertising($index + 1, $advIndex);
-        }
-
+    if ($index >= $maxLoop) {
         return $this->dom->save();
     }
 
+    $currentItem = $items[$index];
+    $nextItem = $items[$index + 1] ?? null;
+
+    static $adIndices = [];
+
+    if (in_array($index, $adIndices)) {
+        return $this->appendSingleAdvertising($index + 1, $advIndex);
+    }
+
+    if (
+        ! preg_match($this->blacklist, $currentItem->outertext) &&
+        (! $nextItem || ! preg_match($this->blacklist, $nextItem->outertext)) &&
+        (! $nextItem || preg_match('/<\w+/', $nextItem->outertext))
+    ) {
+        $currentItem->outertext .= Blade::render('ads-post-parser::ads'.$advIndex);
+        $adIndices[] = $index; // Aggiungi l'indice corrente a $adIndices
+    } else {
+        $this->appendSingleAdvertising($index + 1, $advIndex);
+    }
+
+    return $this->dom->save();
+}
     /**
      * Remove the wrapping div
      */
